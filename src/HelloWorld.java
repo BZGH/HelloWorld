5e21e2300b0e363f1346bb546d9bb6b5b46a9b9a
BZGH <brian.zagorski@clarecontrols.com>
2015-06-15 15:11:17 -0400

    HW#2
    Hello, world!

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+//main: Hello world! output
+	public static void main(String[] args) {
+		System.out.println("Hello, world!"); //Prints one line
+		System.out.println("I'm living the virtual dream!"); //Prints a second line
+	}
+}
+//BZ's Hello, world!
+//Strings are a sequence of characters.
